9a4089acfceb4d2f8aedad652046beb8b38e9b41
galra <galraay@gmail.com>
2017-11-28 00:17:32 +0200

    added plotting and root-path

--- find_params_basic_alg.py
+++ find_params_basic_alg.py
@@ -55,15 +55,15 @@ def gen_pi_map(x1_range, y1_range, pi0=None, resolution=100, iterations=10):
     if x1_range[0] >= x1_range[1] or y1_range[0] >= y1_range[1]:
         raise ValueError('Range boundaries are disordered!')
 
-    for x1 in np.linspace(x1_range[0], x1_range[1], resolution):
-        for y1 in np.linspace(y1_range[0], y1_range[1], resolution):
-            if x1 == 0 or y1 == 0:
-                continue
-            ba.reinitialize(dec(x1), dec(y1), dec(pi0))
-            map_data['x1'].append(x1)
-            map_data['y1'].append(y1)
+    x_space = np.linspace(x1_range[0], x1_range[1], resolution)
+    y_space = np.linspace(y1_range[0], y1_range[1], resolution)
+    map_data['x1'], map_data['y1'] = np.meshgrid(x_space, y_space)
+    map_data['pi'] = np.array(map_data['x1'])
+    for x1_i in range(len(x_space)):
+        for y1_i in range(len(y_space)):
+            ba.reinitialize(dec(x_space[x1_i]), dec(y_space[y1_i]), dec(pi0))
             ba.gen_iterations(iterations)
-            map_data['pi'].append(ba.pis[-1])
+            map_data['pi'][x1_i,y1_i] = (ba.compare_result()**dec(2)).log10()
 
     return map_data
 
@@ -83,19 +83,35 @@ def draw_path(path_data, fig):
     ax = fig.gca(projection='3d')
     ax.plot(path_data['x'], path_data['y'], path_data['pi']) #, label='parametric curve')
 
-def draw_all(x_range=[1, 10], y_range=[1, 10], starting_points=None,
-             pi0=None, resolution=10, iterations=10):
+def draw_all(x_range=[0.1, 2], y_range=[0.1, 2], starting_points=None,
+             pi0=None, resolution=100, iterations=10, zero_threshold=0.001):
     if pi0 is None:
         pi0 = dec(2) + dec(2).sqrt()
     if starting_points is None:
-        starting_points = itertools.product(range(*x_range), range(*y_range))
+        x_space = np.linspace(x_range[0], x_range[1], resolution)
+        y_space = np.linspace(y_range[0], y_range[1], resolution)
+        starting_points = itertools.product(x_space, y_space)
 
     fig = plt.figure()
     map_data = gen_pi_map(x_range, y_range, pi0, resolution, iterations)
     draw_map(map_data, fig)
 
+    # find the path coordinates
+    path_xy = [ (x_space[i], y_space[j])
+                for i,j in itertools.product(range(len(x_space)), range(len(y_space)))
+                if map_data['pi'][i,j] < -3.5 ]
+
+    # zero_path = [ (x_space[i], y_space[j], map_data['pi'][i,j])
+    #               for i,j in i,j in itertools(range(len(x_space)), range(len(y_space)))
+    #               if map_data['pi'][i,j]) < zero_threshold ]
+    path_x, path_y = [ x for x,y in path_xy ], [ y for x,y in path_xy ]
+    fig_path = plt.figure()
+    ax_path = fig_path.gca()
+    ax_path.plot(path_x, path_y, 'b.')
+
     fig.show()
+    fig_path.show()
+    return (path_x, path_y)
 
-
-
-
+def array_to_matlab(array_name, array):
+    return '%s = [%s];' % (array_name, ' '.join([ i.__repr__() for i in array ]))
